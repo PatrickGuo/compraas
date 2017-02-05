9e881db2b0c8eb6f0c5ba36e2feebf2ba4140d14
peizhen guo <patrick.guopz@gmail.com>
2017-02-05 11:41:02 -0500

    initial project.

---
+++ src/main/java/com/peizhen/service/daemon.java
@@ -0,0 +1,15 @@
+package com.peizhen.service;
+
+/**
+ * Created by GPZ on 2/5/17.
+ */
+public class daemon {
+
+
+
+
+    public static void main() {
+
+        System.out.println("Start from here.");
+    }
+}
